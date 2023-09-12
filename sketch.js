b19024b8bb193ee5dc0b11fc1593bde01a517155
Douglas Vanzin <105023407+douglasvanzin@users.noreply.github.com>
2023-09-12 11:50:07 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(600, 400);
+    background(color(0, 0, 0));
+}
+
+function draw() {
+  fill("purple");
+  stroke("blue");
+  
+  if(mouseIsPressed){
+      //rect(mouseX, mouseY, 20, 25);
+    circle(mouseX, mouseY, 15);
+  }
+}
